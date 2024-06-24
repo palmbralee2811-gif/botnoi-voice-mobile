a910f1e0d094f1a87c8ad3f53586b7dac7bf1876
Kawinphop Chomnikorn <kawinphop.ch@kkumail.com>
2024-06-24 18:23:30 +0700

    - Firebase, Flutter, Google Sign-in

--- botnoi_voice_mobile/lib/Screens/AuthScreen/auth_screen.dart
+++ botnoi_voice_mobile/lib/Screens/AuthScreen/auth_screen.dart
@@ -1,10 +1,57 @@
+import 'package:botnoi_voice_mobile/Screens/HomeScreen/home_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:botnoi_voice_mobile/Authentication/authentication_provider.dart';
+import 'package:provider/provider.dart';
 
 class AuthScreen extends StatelessWidget {
   const AuthScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Google Sign-in'),
+        centerTitle: true,
+      ),
+      body: Center(
+        child: Padding(
+          padding: EdgeInsets.all(8.0),
+          child: GestureDetector(
+            onTap: () async {
+              await Provider.of<Authentication>(context, listen: false).signInWithGoogle(context);
+              Navigator.pushReplacement(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => const HomeScreen(),
+                ),
+              );
+            },
+            child: Container(
+              width: double.infinity,
+              height: 45,
+              decoration: BoxDecoration(
+                border: Border.all(
+                  color: Colors.grey,
+                ),
+                borderRadius: BorderRadius.circular(8),
+              ),
+              child: const Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 8.0),
+                    child: Icon(Icons.add),
+                  ),
+                  Text(
+                    'Sign-in with Google Account',
+                    style: TextStyle(fontSize: 17),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
