115211aebdb21d268652509310d9fe471c102948
Kawinphop Chomnikorn <kawinphop.ch@kkumail.com>
2024-07-03 18:34:38 +0700

    1.Responsive UI 2.Select Avatar

---
+++ green/flow4copy/lib/widgets/story_viewer.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+import 'package:story_view/story_view.dart';
+import '../data/data.dart';
+
+class StoryViewer extends StatelessWidget {
+  final List<Story> stories;
+
+  StoryViewer({required this.stories});
+
+  @override
+  Widget build(BuildContext context) {
+    final StoryController controller = StoryController();
+
+    List<StoryItem> storyItems = stories.map((story) {
+      return StoryItem.pageImage(
+        url: story.imageUrl,
+        controller: controller,
+        duration: story.duration,
+        caption: Text(
+          story.userName,
+          style: const TextStyle(
+            fontSize: 24,
+            color: Colors.white,
+          ),
+        ),
+      );
+    }).toList();
+
+    return Scaffold(
+      body: StoryView(
+        storyItems: storyItems,
+        controller: controller,
+        repeat: false,
+        onComplete: () {
+          Navigator.of(context).pop();
+        },
+      ),
+    );
+  }
+}
