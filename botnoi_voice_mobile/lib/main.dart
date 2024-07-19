2afeeefc55ebd4b63b53d22aa9be3952be396b5a
Jira Pit <pitakwongjira@gmail.com>
2024-07-19 21:50:02 +0700

    remove firebase from botnoi-voice-mobile

--- botnoi_voice_mobile/lib/main.dart
+++ botnoi_voice_mobile/lib/main.dart
@@ -1,13 +1,11 @@
 import 'package:botnoi_voice_mobile/Authentication/auth_checker.dart';
 import 'package:botnoi_voice_mobile/Authentication/authentication_provider.dart';
-import 'package:botnoi_voice_mobile/firebase_options.dart';
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  //await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const BotnoiVoiceApp());
 }
 
