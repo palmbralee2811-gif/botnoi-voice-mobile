88c10ef5bc66a4769cdd5d06339f41677540032a
Kawinphop Chomnikorn <kawinphop.ch@kkumail.com>
2024-08-02 19:18:48 +0700

    Update review_widget.dart

--- green/botnoivoice/lib/Widgets/DrawerAppBarWidget/review_widget.dart
+++ green/botnoivoice/lib/Widgets/DrawerAppBarWidget/review_widget.dart
@@ -16,6 +16,7 @@ class _ReviewWidgetState extends State<ReviewWidget> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      resizeToAvoidBottomInset: false,
       backgroundColor: Colors.white,
       appBar: AppBar(
         backgroundColor: Colors.white,
