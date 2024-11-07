7ef538b66bb2512c9885508ef1c9cede1dd7d911
Kawinphop Chomnikorn <kawinphop.chomnikorn@gmail.com>
2024-11-07 17:11:26 +0700

    [Update] Email Login Button

--- green/botnoivoice/lib/presentation/widgets/button/email_login_button.dart
+++ green/botnoivoice/lib/presentation/widgets/button/email_login_button.dart
@@ -40,7 +40,7 @@ class EmailLoginButton extends StatelessWidget {
                   ),
                   SizedBox(width: 16.w),
                   Text(
-                    'เข้าสู่ระบบด้วยชื่อผู้ใช้งานและรหัสผ่าน',
+                    'เข้าสู่ระบบด้วยชื่อผู้ใช้งานหรืออีเมล',
                     style: TextStyle(
                       fontSize: 12.sp,
                       color: kDark,
