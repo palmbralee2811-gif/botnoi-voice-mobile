32ea0c9e217a88c8048c1dbe1301d64638623e25
pongsapat64 <pongsapat.ch.64@ubu.ac.th>
2024-11-21 15:58:32 +0700

    แปล presentation/provider/email เสร็จแล้ว

---
+++ .history/green/botnoivoice/lib/presentation/providers/email/email_delete_account_provider_20241121152429.dart
@@ -0,0 +1,121 @@
+import 'package:botnoivoice/presentation/constants/url.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'package:logger/logger.dart';
+
+/// ***DO NOT DELETE THIS TEXT FOR ANY REASON***
+///
+/// 3 Steps to Delete Account:
+/// - 1. Verify the User Password.
+/// - 2. Delete User Account Data in Database (MongoDB).
+/// - 3. Delete User Account Data in Firebase (Staging and Production).
+///
+/// ***WARNING***
+/// - When you delete the user account in Firebase,
+/// it will delete the user account in both staging and production environments.
+class EmailDeleteAccountProvider with ChangeNotifier {
+  String? _errorMessage;
+  final Logger _logger = Logger(); // For debugging
+
+  /// Getter for error message
+  String? get errorMessage => _errorMessage;
+
+  /// Helper function to check if user is logged in with password provider
+  bool isPasswordProviderUser(User? user) {
+    return user != null &&
+        user.providerData.any((info) => info.providerId == 'password');
+  }
+
+  /// ฟังก์ชันตรวจสอบรหัสผ่าน
+  Future<bool> verifyPassword(String password) async {
+    final user = FirebaseAuth.instance.currentUser;
+    if (isPasswordProviderUser(user)) {
+      try {
+        final credential = EmailAuthProvider.credential(
+          email: user!.email!,
+          password: password,
+        );
+        await user.reauthenticateWithCredential(credential);
+        _errorMessage = null;
+        notifyListeners();
+        return true;
+      } on FirebaseAuthException catch (error) {
+        switch (error.code) {
+          case 'wrong-password':
+            _errorMessage = "รหัสผ่านไม่ถูกต้อง"; //รหัสผ่านไม่ถูกต้อง
+            break;
+          default:
+            _errorMessage = "เกิดข้อผิดพลาดในการยืนยันรหัสผ่าน"; //เกิดข้อผิดพลาดในการยืนยันรหัสผ่าน
+            break;
+        }
+        _logger.e(
+            "Password verification failed. \nMessage: ${error.message} \nCode: ${error.code}");
+        notifyListeners();
+        return false;
+      }
+    } else {
+      _errorMessage = "ไม่ได้เข้าสู่ระบบด้วยชื่อผู้ใช้งานและรหัสผ่าน"; //ไม่ได้เข้าสู่ระบบด้วยชื่อผู้ใช้งานและรหัสผ่าน
+      _logger.e("Cannot verify Google account password");
+      notifyListeners();
+      return false;
+    }
+  }
+
+  /// ฟังก์ชันลบข้อมูลในฐานข้อมูล
+  Future<void> deleteUserAccountWithDatabase() async {
+    final user = FirebaseAuth.instance.currentUser;
+    if (isPasswordProviderUser(user)) {
+      String userId = user!.uid;
+      String url = '$urlDomain/db/dashboard/users/$userId';
+      Map<String, String> headers = {'Content-Type': 'application/json'};
+
+      try {
+        final response = await http.delete(Uri.parse(url), headers: headers);
+        if (response.statusCode == 200) {
+          _errorMessage = null;
+          _logger.d(
+              "User Account deleted successfully with Database \nUser ID: $userId \nEmail: ${user.email}");
+        } else {
+          _errorMessage =
+              'ไม่สามารถลบบัญชีผู้ใช้ได้ รหัสสถานะ: ${response.statusCode}'; //ไม่สามารถลบบัญชีผู้ใช้ได้ รหัสสถานะ:
+          _logger.e(
+              "Failed to delete user account. Status Code: ${response.statusCode}");
+          notifyListeners();
+        }
+      } catch (error) {
+        _errorMessage = "เกิดข้อผิดพลาดในการลบบัญชี: $error"; //เกิดข้อผิดพลาดในการลบบัญชี
+        _logger.e("Error deleting user account: $error");
+        notifyListeners();
+      }
+    } else {
+      _errorMessage = "ไม่ได้เข้าสู่ระบบด้วยผู้ใช้และรหัสผ่าน"; //ไม่ได้เข้าสู่ระบบด้วยผู้ใช้และรหัสผ่าน
+      _logger.w("No user is currently signed in.");
+      notifyListeners();
+    }
+  }
+
+  /// ฟังก์ชันลบข้อมูลใน Firebase
+  Future<void> deleteUserAccountWithFirebase(BuildContext context) async {
+    final user = FirebaseAuth.instance.currentUser;
+    if (isPasswordProviderUser(user)) {
+      try {
+        await user!.delete();
+        _errorMessage = null;
+        _logger.i(
+            "User Account deleted successfully from Firebase \nEmail: ${user.email} \nUser ID: ${user.uid}");
+        ScaffoldMessenger.of(context).showSnackBar(
+          const SnackBar(content: Text('ลบบัญชีสำเร็จ')),
+        );
+      } catch (error) {
+        _errorMessage = "เกิดข้อผิดพลาดในการลบบัญชี: $error";
+        _logger.e("Error deleting account: $error");
+        notifyListeners();
+      }
+    } else {
+      _errorMessage = "ไม่ได้เข้าสู่ระบบด้วยผู้ใช้งานและรหัสผ่าน";
+      _logger.e("No user logged in or invalid provider for Firebase deletion");
+      notifyListeners();
+    }
+  }
+}
---
+++ .history/green/botnoivoice/lib/presentation/providers/email/email_login_provider_20241121154117.dart
@@ -0,0 +1,141 @@
+import 'package:botnoivoice/presentation/providers/email/email_token_provider.dart';
+import 'package:easy_localization/easy_localization.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/scheduler.dart';
+import 'package:logger/logger.dart';
+import 'package:provider/provider.dart';
+import 'package:botnoivoice/presentation/providers/email/email_username_api_provider.dart';
+
+class EmailLoginProvider with ChangeNotifier {
+  User? user;
+  String? _errorMessage;
+  final Logger _logger = Logger(); // สำหรับ debug
+  bool _isLoggedIn = false;
+
+  /// Getter สำหรับการตรวจสอบว่าเข้าสู่ระบบแล้วหรือไม่
+  bool get isLoggedIn => _isLoggedIn;
+
+  /// Getter สำหรับการตรวจสอบการยืนยันตัวตน
+  bool get isAuthenticated {
+    return FirebaseAuth.instance.currentUser?.uid != null &&
+        user?.providerData.isNotEmpty == true &&
+        user?.providerData[0].providerId == 'password';
+  }
+
+  /// Getter สำหรับอีเมล
+  String? get getUserEmail => user?.email;
+
+  /// Getter สำหรับ error message
+  String? get errorMessage => _errorMessage;
+
+  EmailLoginProvider() {
+    FirebaseAuth.instance.authStateChanges().listen((User? user) async {
+      if (user?.providerData[0].providerId == 'password') {
+        this.user = user;
+        _logger.i("Login with Email: $user");
+        _isLoggedIn = true;
+      }
+      notifyListeners(); // Update UI
+    });
+  }
+
+  /// เข้าสู่ระบบด้วย `username` และ `password` พร้อมกับเรียก `get email by username`
+  Future<void> loginWithUsernamePassword(
+      String username, String password, BuildContext context) async {
+    final emailUsernameProvider =
+        Provider.of<EmailUsernameApiProvider>(context, listen: false);
+
+    try {
+      // เรียกใช้ฟังก์ชัน get email by username เพื่อดึง email จาก username
+      await emailUsernameProvider.getEmailByUsername(username);
+      final email = emailUsernameProvider.result; // รับค่า email จาก result
+
+      if (email == 'email not found') {
+        _errorMessage = 'app_drawer.invalid_username'.tr(); //ชื่อผู้ใช้งานไม่ถูกต้อง
+        _logger.e("No email found for username: $username");
+        notifyListeners();
+        return;
+      }
+
+      // ทำการเข้าสู่ระบบโดยใช้ email และ password ที่ได้จาก username
+      await loginWithEmailPassword(email, password);
+    } catch (e) {
+      _errorMessage = "${'app_drawer.error_logging_in'.tr()} $e"; //เกิดข้อผิดพลาดในการเข้าสู่ระบบ:
+      _logger.e("Error logging in with username: $e");
+      notifyListeners();
+    }
+  }
+
+  /// เข้าสู่ระบบด้วยอีเมลและรหัสผ่าน
+  Future<void> loginWithEmailPassword(String email, String password) async {
+    try {
+      UserCredential userCredential = await FirebaseAuth.instance
+          .signInWithEmailAndPassword(email: email, password: password);
+      _errorMessage = null;
+
+      // ตรวจสอบว่าผู้ใช้อีเมลได้รับการยืนยันหรือไม่
+      if (!userCredential.user!.emailVerified) {
+        _errorMessage = "กรุณายืนยันอีเมลก่อนเข้าสู่ระบบ."; //กรุณายืนยันอีเมลก่อนเข้าสู่ระบบ
+        _logger.w("User email is not verified: $email");
+
+        // ส่งอีเมลยืนยันหากยังไม่ได้รับการยืนยัน
+        try {
+          await userCredential.user?.sendEmailVerification();
+          _errorMessage = "กรุณายืนยันอีเมล: $email"; //กรุณายืนยันอีเมล:
+          _logger.i("Verification email sent to: $email");
+        } on FirebaseAuthException catch (e) {
+          _errorMessage = e.message;
+          _logger.e("Failed to send verification email: $e");
+        }
+
+        // ทำการ sign out เพื่อป้องกันการเข้าถึงโดยไม่ยืนยันอีเมล
+        await FirebaseAuth.instance.signOut();
+        _isLoggedIn = false;
+        notifyListeners();
+        return;
+      }
+
+      // หากยืนยันอีเมลแล้ว อนุญาตให้เข้าสู่ระบบ
+      user = userCredential.user;
+      _isLoggedIn = true;
+      _logger.i(
+          "User logged in successfully with email: $email, Email User ID: ${user?.uid}");
+      notifyListeners();
+    } on FirebaseAuthException catch (e) {
+      switch (e.code) {
+        case 'invalid-email':
+          _errorMessage = "อีเมลไม่ถูกต้อง"; //อีเมลไม่ถูกต้อง
+          break;
+        case 'wrong-password':
+          _errorMessage = "รหัสผ่านไม่ถูกต้อง"; //รหัสผ่านไม่ถูกต้อง
+          break;
+        case 'user-disabled':
+          _errorMessage = "บัญชีนี้ถูกระงับการใช้งาน"; //บัญชีนี้ถูกระงับการใช้งาน
+          break;
+        default:
+          _errorMessage = "ชื่อผู้ใช้งานหรือรหัสผ่านไม่ถูกต้อง"; //ชื่อผู้ใช้งานหรือรหัสผ่านไม่ถูกต้อง
+          break;
+      }
+      _logger.e(
+          "Error logging in with email. \nMessage: ${e.message} \nCode: ${e.code}");
+      notifyListeners();
+    }
+  }
+
+  /// Sign out for Login with Email and Password
+  Future<void> signOutWithEmail(BuildContext context) async {
+    try {
+      Provider.of<EmailTokenProvider>(context, listen: false).clearTokens();
+      await FirebaseAuth.instance.signOut();
+      _isLoggedIn = false;
+      _logger.i("User signed out successfully");
+    } catch (e) {
+      _logger.e("Error signing out: $e");
+    }
+
+    SchedulerBinding.instance.addPostFrameCallback((_) {
+      notifyListeners();
+    });
+  }
+}
