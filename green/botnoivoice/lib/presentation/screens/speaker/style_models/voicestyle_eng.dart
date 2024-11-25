87b20c7b955c09c104d4983cb8706bca4b0220f6
KiadtisakTH <Kiadtisak.th.64@ubu.ac.th>
2024-11-25 17:54:51 +0700

    [Fix] style_models  prototype reset 1 and 2

---
+++ green/botnoivoice/lib/presentation/screens/speaker/style_models/voicestyle_eng.dart
@@ -0,0 +1,20 @@
+    // รายการสไตล์เสียงภาษาอังกฤษ
+  List<String> voiceStyleEng = [
+    'Playful',
+    'Serious',
+    'Clear',
+    'Excited',
+    'Deep',
+    'Regional',
+    'Soft',
+    'Smooth',
+    'Cute',
+    'Trustworthy',
+    'Confident',
+    'Sweet',
+    'Warm',
+    'Northeastern',
+    'Northern',
+    'Calm',
+    'Southern'
+  ];
