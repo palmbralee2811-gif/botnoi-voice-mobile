52f6a17d81733a8c651945ae40a014eaa9855a0b
Kawinphop Chomnikorn <kawinphop.chomnikorn@gmail.com>
2024-12-27 10:50:16 +0700

    [Add] Desktop App

---
+++ green/botnoivoice/macos/Pods/RevenueCat/Sources/Paywalls/Components/PaywallStackComponent.swift
@@ -0,0 +1,76 @@
+//
+//  Copyright RevenueCat Inc. All Rights Reserved.
+//
+//  Licensed under the MIT License (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//      https://opensource.org/licenses/MIT
+//
+//  StackComponent.swift
+//
+//  Created by James Borthwick on 2024-08-20.
+// swiftlint:disable missing_docs
+
+import Foundation
+
+#if PAYWALL_COMPONENTS
+
+public extension PaywallComponent {
+
+    // The most common type of stack that can contain any type of PaywallComponent
+    typealias StackComponent = GenericStackComponent<PaywallComponent>
+
+    // A specialized stack that can only hold PaywallComponent.PackageComponent
+    typealias PackagesStackComponent = GenericStackComponent<PaywallComponent.PackageComponent>
+
+    struct GenericStackComponent<T: StackableComponent>: PaywallComponentBase {
+
+        let type: ComponentType
+        public let components: [T]
+        public let width: WidthSize?
+        public let spacing: CGFloat?
+        public let backgroundColor: ColorInfo?
+        public let dimension: Dimension
+        public let padding: Padding
+        public let margin: Padding
+        public let cornerRadiuses: CornerRadiuses?
+        public let border: Border?
+
+        public init(components: [T],
+                    dimension: Dimension = .vertical(.center),
+                    width: WidthSize? = nil,
+                    spacing: CGFloat? = nil,
+                    backgroundColor: ColorInfo? = nil,
+                    padding: Padding = .zero,
+                    margin: Padding = .zero,
+                    cornerRadiuses: CornerRadiuses? = nil,
+                    border: Border? = nil
+        ) {
+            self.components = components
+            self.width = width
+            self.spacing = spacing
+            self.backgroundColor = backgroundColor
+            self.type = .stack
+            self.dimension = dimension
+            self.padding = padding
+            self.margin = margin
+            self.cornerRadiuses = cornerRadiuses
+            self.border = border
+        }
+
+    }
+
+}
+
+public extension PaywallComponent {
+
+    // Components that can be contained in a stack
+    protocol StackableComponent: PaywallComponentBase {}
+
+}
+
+extension PaywallComponent: PaywallComponent.StackableComponent {}
+extension PaywallComponent.PackageComponent: PaywallComponent.StackableComponent {}
+
+#endif
