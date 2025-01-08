3f874193a7a26c84d8364d3334b870e2ccf017f0
pongsapat64 <pongsapat.ch.64@ubu.ac.th>
2025-01-08 15:14:57 +0700

    [Fix] overflow UI

--- green/botnoivoice/lib/presentation/screens/appbar/appbar_bottom.dart
+++ green/botnoivoice/lib/presentation/screens/appbar/appbar_bottom.dart
@@ -39,7 +39,7 @@ class _AppBarBottomState extends State<AppBarBottom> {
     final speakerProvider = Provider.of<SpeakerRepositoryImpl>(context);
     return SizedBox(
       width: double.infinity,
-      height: OrientationHelper.isLandscape ? 80.h : 42.h,
+      height: OrientationHelper.isLandscape ? 80.h : 60.h,
       child: Row(
         mainAxisAlignment: MainAxisAlignment.start,
         children: [
