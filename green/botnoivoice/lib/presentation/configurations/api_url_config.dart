99097464937af39999899e8786195949fa40907b
Kawinphop Chomnikorn <kawinphop.chomnikorn@gmail.com>
2025-01-17 18:33:13 +0700

    [Fix] Run App on Debugging Mode

--- green/botnoivoice/lib/presentation/configurations/api_url_config.dart
+++ green/botnoivoice/lib/presentation/configurations/api_url_config.dart
@@ -7,4 +7,4 @@ String _productionUrl = "https://api-voice.botnoi.ai";
 String _stagingUrl = "https://api-voice-staging.botnoi.ai";
 
 /// Returns the API URL for the current environment (Production or Staging)
-String get apiUrl => _productionUrl;
+String get apiUrl => _stagingUrl;
