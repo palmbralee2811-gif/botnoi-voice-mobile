5d82164f743b6f5f7eda53c194e423bb1c07e0da
KiadtisakTH <Kiadtisak.th.64@ubu.ac.th>
2025-01-30 18:51:44 +0700

    [Fix] เเก้ไข responsive หน้า language_change_bottom_sheet_app_drawer

--- green/botnoivoice/lib/presentation/widgets/language/language_change_bottom_sheet_app_drawer.dart
+++ green/botnoivoice/lib/presentation/widgets/language/language_change_bottom_sheet_app_drawer.dart
@@ -15,143 +15,147 @@ void showLanguageBottomSheet({
   showModalBottomSheet(
     context: context,
     isScrollControlled: true,
-    backgroundColor: Colors.transparent, 
-    shape: RoundedRectangleBorder(
-      borderRadius: BorderRadius.vertical(top: Radius.circular(20.r)),
-    ),
+    backgroundColor: Colors.transparent,
     builder: (BuildContext context) {
-      return Align( // ✅ ติดขอบล่างของจอ
-        alignment: Alignment.bottomCenter, 
-        child: FractionallySizedBox(
-          heightFactor: 0.4, // ✅ ปรับขนาด (40% ของหน้าจอ)
-          child: ClipRRect(
+      return GestureDetector(
+        onTap: () => Navigator.pop(context), 
+        behavior: HitTestBehavior.opaque, 
+        child: Container(
+          width: double.infinity,
+          height: OrientationHelper.isLandscape ? 280.h : 180.h,
+          decoration: BoxDecoration(
+            color: Colors.white,
             borderRadius: BorderRadius.vertical(top: Radius.circular(20.r)),
-            child: Container(
-              width: double.infinity,
-              decoration: BoxDecoration(
-                color: Colors.white,
-                borderRadius: BorderRadius.vertical(top: Radius.circular(20.r)),
-              ),
-              child: Column(
-                mainAxisSize: MainAxisSize.min,
-                children: [
-                  Padding(
-                    padding: EdgeInsets.symmetric(horizontal: 20.w, vertical: 15.h),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Text(
-                          'language'.tr(),
-                          style: GoogleFonts.prompt(
-                            fontSize: OrientationHelper.isLandscape ? 12.sp : 16.sp,
-                            fontWeight: FontWeight.w600,
-                          ),
-                        ),
-                        InkWell(
-                          onTap: () {
-                            Navigator.pop(context);
-                          },
-                          child: Icon(
-                            Icons.close,
-                            size: OrientationHelper.isLandscape ? 16.sp : 24.sp,
-                            color: Colors.black,
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
-              
-                  Flexible(
-                    child: SingleChildScrollView(
-                      physics: const BouncingScrollPhysics(),
-                      child: Column(
-                        children: [
-                          SizedBox(height: 16.h),
-
-                          // ✅ ปุ่มเลือกภาษาไทย
-                          Material(
-                            color: Colors.transparent, 
-                            child: InkWell(
-                              onTap: () async {
-                                await saveSelectedLanguage('th');
-                                onLanguageSelected('th');
-                                context.setLocale(const Locale('th'));
-                                Navigator.pushReplacement(
-                                    context,
-                                    MaterialPageRoute(
-                                        builder: (BuildContext context) =>
-                                            const HomeScreen()));
-                              },
-                              highlightColor: Colors.grey[300], 
-                              splashColor: Colors.transparent, 
-                              borderRadius: BorderRadius.circular(8.r),
-                              child: Padding(
-                                padding: EdgeInsets.symmetric(vertical: 8.h, horizontal: 20.w),
-                                child: Row(
-                                  children: [
-                                    Image.asset('assets/images/national_flag/thai.png',
-                                        width: 26.w, height: 26.h),
-                                    SizedBox(width: 12.w),
-                                    Text(
-                                      'ไทย',
-                                      style: GoogleFonts.prompt(
-                                        fontSize: 14.sp,
-                                        fontWeight: selectedLanguage == 'th' ? FontWeight.w600 : FontWeight.w400,
-                                      ),
-                                    ),
-                                  ],
-                                ),
-                              ),
-                            ),
-                          ),
-
-                          SizedBox(height: 16.h),
-
-                          // ✅ ปุ่มเลือกภาษาอังกฤษ
-                          Material(
-                            color: Colors.transparent, 
-                            child: InkWell(
-                              onTap: () async {
-                                await saveSelectedLanguage('en');
-                                onLanguageSelected('en');
-                                context.setLocale(const Locale('en'));
-                                Navigator.pushReplacement(
-                                    context,
-                                    MaterialPageRoute(
-                                        builder: (BuildContext context) =>
-                                            const HomeScreen()));
-                              },
-                              highlightColor: Colors.grey[300], 
-                              splashColor: Colors.transparent, 
-                              borderRadius: BorderRadius.circular(8.r),
-                              child: Padding(
-                                padding: EdgeInsets.symmetric(vertical: 8.h, horizontal: 20.w),
-                                child: Row(
-                                  children: [
-                                    Image.asset('assets/images/national_flag/english.png',
-                                        width: 26.w, height: 26.h),
-                                    SizedBox(width: 12.w),
-                                    Text(
-                                      'English',
-                                      style: GoogleFonts.prompt(
-                                        fontSize: 14.sp,
-                                        fontWeight: selectedLanguage == 'en' ? FontWeight.w600 : FontWeight.w400,
-                                      ),
-                                    ),
-                                  ],
-                                ),
-                              ),
-                            ),
-                          ),
-
-                          SizedBox(height: 16.h),
-                        ],
+          ),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Padding(
+                padding: EdgeInsets.symmetric(horizontal: 20.w, vertical: 15.h),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      'language'.tr(),
+                      style: GoogleFonts.prompt(
+                        fontSize: OrientationHelper.isLandscape ? 12.sp : 16.sp,
+                        fontWeight: FontWeight.w600,
                       ),
                     ),
-                  ),
-                ],
+                    InkWell(
+                      onTap: () {
+                        Navigator.pop(context);
+                      },
+                      child: Icon(
+                        Icons.close,
+                        size: OrientationHelper.isLandscape ? 14.sp : 24.sp,
+                        color: Colors.black,
+                      ),
+                    ),
+                  ],
+                ),
               ),
-            ),
+
+
+              Flexible(
+                child: SingleChildScrollView(
+                  physics: const BouncingScrollPhysics(),
+                  child: Column(
+                    children: [
+                      SizedBox(height: 16.h),
+
+                     
+                      Material(
+                        color: Colors.transparent,
+                        child: InkWell(
+                          onTap: () async {
+                            await saveSelectedLanguage('th');
+                            onLanguageSelected('th');
+                            context.setLocale(const Locale('th'));
+
+                            
+                            Navigator.of(context).pop(); // ปิด Bottom Sheet
+                            Navigator.of(context).popUntil((route) => route.isFirst); // ปิด Drawer
+                            
+                            
+                            Navigator.pushReplacement(
+                              context,
+                              MaterialPageRoute(builder: (context) => const HomeScreen()),
+                            );
+                          },
+                          highlightColor: Colors.grey[300],
+                          splashColor: Colors.transparent,
+                          borderRadius: BorderRadius.circular(8.r),
+                          child: Padding(
+                            padding: EdgeInsets.symmetric(vertical: 8.h, horizontal: 20.w),
+                            child: Row(
+                              children: [
+                                Image.asset('assets/images/national_flag/thai.png',
+                                    width: 26.w, height: 26.h),
+                                SizedBox(width: 12.w),
+                                Text(
+                                  'ไทย',
+                                  style: GoogleFonts.prompt(
+                                    fontSize: OrientationHelper.isLandscape ? 12.sp : 14.sp,
+                                    fontWeight: selectedLanguage == 'th' ? FontWeight.w600 : FontWeight.w400,
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ),
+                      ),
+
+                      SizedBox(height: 16.h),
+
+                  
+                      Material(
+                        color: Colors.transparent,
+                        child: InkWell(
+                          onTap: () async {
+                            await saveSelectedLanguage('en');
+                            onLanguageSelected('en');
+                            context.setLocale(const Locale('en'));
+
+                            
+                            Navigator.of(context).pop(); // ปิด Bottom Sheet
+                            Navigator.of(context).popUntil((route) => route.isFirst); // ปิด Drawer
+                            
+                        
+                            Navigator.pushReplacement(
+                              context,
+                              MaterialPageRoute(builder: (context) => const HomeScreen()),
+                            );
+                          },
+                          highlightColor: Colors.grey[300],
+                          splashColor: Colors.transparent,
+                          borderRadius: BorderRadius.circular(8.r),
+                          child: Padding(
+                            padding: EdgeInsets.symmetric(vertical: 8.h, horizontal: 20.w),
+                            child: Row(
+                              children: [
+                                Image.asset('assets/images/national_flag/english.png',
+                                    width: 26.w, height: 26.h),
+                                SizedBox(width: 12.w),
+                                Text(
+                                  'English',
+                                  style: GoogleFonts.prompt(
+                                    fontSize: OrientationHelper.isLandscape ? 12.sp : 14.sp,
+                                    fontWeight: selectedLanguage == 'en' ? FontWeight.w600 : FontWeight.w400,
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ),
+                      ),
+
+                      SizedBox(height: 16.h),
+                    ],
+                  ),
+                ),
+              ),
+            ],
           ),
         ),
       );
