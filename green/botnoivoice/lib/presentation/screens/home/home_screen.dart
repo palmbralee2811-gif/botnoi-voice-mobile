f52fafa2c11a455bc8563cbca78bc8456dc367b0
Kawinphop Chomnikorn <kawinphop.chomnikorn@gmail.com>
2025-01-31 14:29:19 +0700

    Update home_screen.dart

--- green/botnoivoice/lib/presentation/screens/home/home_screen.dart
+++ green/botnoivoice/lib/presentation/screens/home/home_screen.dart
@@ -100,11 +100,12 @@ class _HomeScreenState extends State<HomeScreen> {
     if (creditsProvider.remainingQuotaDownload == "0" && !hasShownQuotaDialog) {
       hasShownQuotaDialog = true; // Show dialog only once
       NotificationDialog(
-              context: context,
-              //TODO: แปลงข้อความเป็น ภาษาอังกฤษ
-              text:'คุณใช้โควต้าฟรี 10 ครั้ง/วันครบแล้ว หลังจาก นี้ระบบจะเริ่มหักพ้อยท์ตามการใช้งาน',
-              onPressed: () {})
-          .showCheckmarkModalWithAction(context);
+        context: context,
+        //TODO: แปลงข้อความเป็น ภาษาอังกฤษ
+        text:
+            'คุณใช้โควต้าฟรี 10 ครั้ง/วันครบแล้ว หลังจาก นี้ระบบจะเริ่มหักพ้อยท์ตามการใช้งาน',
+        onPressed: () {},
+      ).showCheckmarkModalWithAction(context);
     } else {
       await _generateAudioConfirmed();
     }
