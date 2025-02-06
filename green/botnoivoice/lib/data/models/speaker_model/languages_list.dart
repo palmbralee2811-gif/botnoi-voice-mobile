1f0b35befe56a767c5422e8da7750e14edbbf538
KiadtisakTH <Kiadtisak.th.64@ubu.ac.th>
2025-02-06 14:48:33 +0700

    [update] เพิ่มตัวเเปร indonesianName

--- green/botnoivoice/lib/data/models/speaker_model/languages_list.dart
+++ green/botnoivoice/lib/data/models/speaker_model/languages_list.dart
@@ -4,154 +4,154 @@ final languages = [
   {
     'thaiName': 'ไทย',
     'englishName': 'Thai',
-    'indonesianName': 'Bahasa',
+    'indonesianName': 'Bahasa Thai',
     'image': 'assets/images/national_flag/thai.png',
     'code': 'TH'
   },
   {
     'thaiName': 'อังกฤษ',
     'englishName': 'English',
-    'indonesianName': 'Inggris',
+    'indonesianName': 'Bahasa Inggris',
     'image': 'assets/images/national_flag/english.png',
     'code': 'EN'
   },
   {
     'thaiName': 'อินโดนีเซีย',
     'englishName': 'Indonesian',
-    'indonesianName': 'XYZ0123',
+    'indonesianName': 'Bahasa Indonesia',
     'image': 'assets/images/national_flag/indonesian.png',
     'code': 'ID'
   },
   {
     'thaiName': 'ญี่ปุ่น',
     'englishName': 'Japanese',
-    'indonesianName': 'XYZ0123',
+    'indonesianName': 'Bahasa Jepang',
     'image': 'assets/images/national_flag/japanese.png',
     'code': 'JA'
   },
   {
     'thaiName': 'ลาว',
     'englishName': 'Lao',
-    'indonesianName': 'XYZ0123',
+    'indonesianName': 'Bahasa Lao',
     'image': 'assets/images/national_flag/lao.png',
     'code': 'LO'
   },
   {
     'thaiName': 'เมียนมาร์',
     'englishName': 'Burmese',
-    'indonesianName': 'XYZ0123',
+    'indonesianName': 'Bahasa Myanmar',
     'image': 'assets/images/national_flag/burmese.png',
     'code': 'MY'
   },
   {
     'thaiName': 'เวียดนาม',
     'englishName': 'Vietnamese',
-    'indonesianName': 'XYZ0123',
+    'indonesianName': 'Bahasa Vietnam',
     'image': 'assets/images/national_flag/vietnamese.png',
     'code': 'VI'
   },
   {
     'thaiName': 'จีน',
     'englishName': 'Chinese',
-    'indonesianName': 'XYZ0123',
+    'indonesianName': 'Bahasa Mandarin',
     'image': 'assets/images/national_flag/chinese.png',
     'code': 'ZH'
   },
   {
     'thaiName': 'กัมพูชา',
     'englishName': 'Cambodia',
-    'indonesianName': 'XYZ0123',
+    'indonesianName': 'Bahasa Khmer',
     'image': 'assets/images/national_flag/cambodia.png',
     'code': 'KM'
   },
   {
     'thaiName': 'ฟิลิปปินส์',
     'englishName': 'Filipino',
-    'indonesianName': 'XYZ0123',
+    'indonesianName': 'Bahasa Filipino',
     'image': 'assets/images/national_flag/filipino.png',
     'code': 'FIL'
   },
   {
     'thaiName': 'อาหรับ',
     'englishName': 'Arabic',
-    'indonesianName': 'XYZ0123',
+    'indonesianName': 'Bahasa Arab',
     'image': 'assets/images/national_flag/arabic.png',
     'code': 'AR'
   },
   {
     'thaiName': 'เยอรมัน',
     'englishName': 'German',
-    'indonesianName': 'XYZ0123',
+    'indonesianName': 'Bahasa Jerman',
     'image': 'assets/images/national_flag/german.png',
     'code': 'DE'
   },
   {
     'thaiName': 'สเปน',
     'englishName': 'Spanish',
-    'indonesianName': 'XYZ0123',
+    'indonesianName': 'Bahasa Spanyol',
     'image': 'assets/images/national_flag/spanish.png',
     'code': 'ES'
   },
   {
     'thaiName': 'ฝรั่งเศส',
     'englishName': 'French',
-    'indonesianName': 'XYZ0123',
+    'indonesianName': 'Bahasa Prancis',
     'image': 'assets/images/national_flag/french.png',
     'code': 'FR'
   },
   {
     'thaiName': 'ดัตช์',
     'englishName': 'Dutch',
-    'indonesianName': 'XYZ0123',
+    'indonesianName': 'Bahasa Belanda',
     'image': 'assets/images/national_flag/dutch.png',
     'code': 'NL'
   },
   {
     'thaiName': 'เกาหลี',
     'englishName': 'Korean',
-    'indonesianName': 'XYZ0123',
+    'indonesianName': 'Bahasa Korea',
     'image': 'assets/images/national_flag/korean.png',
     'code': 'KO'
   },
   {
     'thaiName': 'มาเลเซีย',
     'englishName': 'Malaysian',
-    'indonesianName': 'XYZ0123',
+    'indonesianName': 'Bahasa Malaysia',
     'image': 'assets/images/national_flag/malaysian.png',
     'code': 'MS'
   },
   {
     'thaiName': 'โปรตุเกส',
     'englishName': 'Portuguese',
-    'indonesianName': 'XYZ0123',
+    'indonesianName': 'Bahasa Portugis',
     'image': 'assets/images/national_flag/portuguese.png',
     'code': 'PT-BR'
   },
   {
     'thaiName': 'รัสเซีย',
     'englishName': 'Russian',
-    'indonesianName': 'XYZ0123',
+    'indonesianName': 'Bahasa Rusia',
     'image': 'assets/images/national_flag/russian.png',
     'code': 'RU'
   },
   {
     'thaiName': 'ฮินดี',
     'englishName': 'Hindi',
-    'indonesianName': 'XYZ0123',
+    'indonesianName': 'Bahasa Hindi',
     'image': 'assets/images/national_flag/hindi.png',
     'code': 'HI'
   },
   {
     'thaiName': 'อิตาลี',
     'englishName': 'Italian',
-    'indonesianName': 'XYZ0123',
+    'indonesianName': 'Bahasa Italia',
     'image': 'assets/images/national_flag/italian.png',
     'code': 'IT'
   },
   {
     'thaiName': 'ตุรกี',
     'englishName': 'Turkish',
-    'indonesianName': 'XYZ0123',
+    'indonesianName': 'Bahasa Turki',
     'image': 'assets/images/national_flag/turkish.png',
     'code': 'TR'
   },
