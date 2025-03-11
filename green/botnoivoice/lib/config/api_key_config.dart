c15f7698bbc1eabd71ce0a0d4ad98201c28a8807
Kawinphop Chomnikorn <kawinphop.chomnikorn@gmail.com>
2025-03-11 12:25:22 +0700

    Rename

--- green/botnoivoice/lib/config/api_key_config.dart
+++ green/botnoivoice/lib/config/api_key_config.dart
@@ -1,8 +1,8 @@
 // LINE Login Key for Production Mode
-// String _lineSdkChannelId = "1656375389";
+String _lineSdkChannelId = "1656375389";
 
 // LINE Login Key for Staging Mode
-String _lineSdkChannelId = "2003460314";
+// String _lineSdkChannelId = "2003460314";
 
 String _emailUsernameApiKey = "Ym90b25vaQ";
 String _appleRevenueCatApiKey = "appl_mImKODlZTfTPWctkaQrVLpkMUuz";
