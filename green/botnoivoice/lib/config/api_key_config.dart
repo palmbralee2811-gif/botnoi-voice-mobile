2dac6c92d4a8533c4d950e701ccae7fea7d7b263
Kawinphop Chomnikorn <kawinphop.chomnikorn@gmail.com>
2025-03-11 10:11:21 +0700

    LINE Login Key 2 Mode

--- green/botnoivoice/lib/config/api_key_config.dart
+++ green/botnoivoice/lib/config/api_key_config.dart
@@ -1,8 +1,8 @@
-// on Staging
-// String _lineSdkChannelId = "2003460314";
+// LINE Login Key for Production Mode
+// String _lineSdkChannelId = "1656375389";
 
-// on Production
-String _lineSdkChannelId = "1656375389";
+// LINE Login Key for Staging Mode
+String _lineSdkChannelId = "2003460314";
 
 String _emailUsernameApiKey = "Ym90b25vaQ";
 String _appleRevenueCatApiKey = "appl_mImKODlZTfTPWctkaQrVLpkMUuz";
