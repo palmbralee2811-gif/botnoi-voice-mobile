1eebc72b88facb38b1e5c17e90c5aa4daea5db89
korarit <krtza1@gmail.com>
2025-05-06 12:45:23 +0700

    [Fix] update audio player implementation to use just_audio package for improved audio handling

--- botnoivoice/lib/screen/main/home/widget/audio_player_dialog.dart
+++ botnoivoice/lib/screen/main/home/widget/audio_player_dialog.dart
@@ -1,7 +1,7 @@
 import 'dart:io';
 import 'dart:isolate';
 import 'dart:ui';
-import 'package:audioplayers/audioplayers.dart';
+import 'package:just_audio/just_audio.dart';
 import 'package:botnoivoice/screen/responsive/responsive_design_orientation.dart';
 import 'package:botnoivoice/screen/main/home/function/share_audio_file.dart';
 import 'package:botnoivoice/screen/main/home/function/create_ios_app_folder.dart';
@@ -75,39 +75,41 @@ class _AudioPlayerDialogState extends State<AudioPlayerDialog> {
     try {
       final file = File(widget.filePath);
       if (!await file.exists() || await file.length() == 0) {
-        throw Exception('audio_player.audio_file_not_found'
-            .tr()); //ไม่พบไฟล์เสียงหรือไฟล์ว่างเปล่า
+        throw Exception('audio_player.audio_file_not_found'.tr());
       }
-      await audioPlayer.setSourceDeviceFile(widget.filePath);
-      audioPlayer.onDurationChanged.listen((d) {
-        if (mounted) {
+      await audioPlayer.setFilePath(widget.filePath);
+
+      // ฟัง duration
+      audioPlayer.durationStream.listen((d) {
+        if (mounted && d != null) {
           setState(() {
             duration = d;
           });
         }
       });
-      audioPlayer.onPositionChanged.listen((p) {
+
+      // ฟัง position
+      audioPlayer.positionStream.listen((p) {
         if (mounted) {
           setState(() {
             position = p;
           });
         }
       });
-      audioPlayer.onPlayerStateChanged.listen((state) {
+
+      // ฟังสถานะการเล่น
+      audioPlayer.playerStateStream.listen((state) {
         if (mounted) {
           setState(() {
-            isPlaying = state == PlayerState.playing;
+            isPlaying = state.playing;
+            if (state.processingState == ProcessingState.completed) {
+              position = Duration.zero;
+              isPlaying = false;
+            }
           });
         }
       });
-      audioPlayer.onPlayerComplete.listen((event) {
-        if (mounted) {
-          setState(() {
-            position = Duration.zero;
-            isPlaying = false;
-          });
-        }
-      });
+
       if (mounted) {
         setState(() {
           isLoading = false;
@@ -115,13 +117,10 @@ class _AudioPlayerDialogState extends State<AudioPlayerDialog> {
       }
     } catch (e) {
       if (mounted) {
-        // Close Audio Player Dialog
         context.pop();
-
         ScaffoldMessenger.of(context).showSnackBar(
           SnackBar(
-              content: Text(
-                  "${'audio_player.error_playing_audio'.tr()} $e")), //เกิดข้อผิดพลาดในการเล่นไฟล์เสียง:
+              content: Text("${'audio_player.error_playing_audio'.tr()} $e")),
         );
       }
     }
@@ -251,15 +250,17 @@ class _AudioPlayerDialogState extends State<AudioPlayerDialog> {
                           if (isPlaying) {
                             await audioPlayer.pause();
                           } else {
-                            await audioPlayer
-                                .play(DeviceFileSource(widget.filePath));
+                            if (audioPlayer.processingState ==ProcessingState.completed) {
+                              await audioPlayer.seek(Duration.zero);
+                            }
+                            await audioPlayer.play();
                           }
                         },
                       ),
                       SizedBox(width: 10.w),
                       Expanded(
                         child: StreamBuilder<Duration>(
-                          stream: audioPlayer.onPositionChanged,
+                          stream: audioPlayer.positionStream,
                           builder: (context, snapshot) {
                             final position = snapshot.data ?? Duration.zero;
                             return Slider(
