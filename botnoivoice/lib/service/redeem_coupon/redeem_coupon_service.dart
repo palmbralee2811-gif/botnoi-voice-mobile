5a58ca87524718adc60bf50485b415fc073aa632
korarit <krtza1@gmail.com>
2025-05-28 14:34:33 +0700

    [Refactor] remove unused import for dart:math in RedeemCouponService

--- botnoivoice/lib/service/redeem_coupon/redeem_coupon_service.dart
+++ botnoivoice/lib/service/redeem_coupon/redeem_coupon_service.dart
@@ -1,5 +1,4 @@
 import 'dart:convert';
-import 'dart:math';
 import 'package:botnoivoice/config/api_url_config.dart';
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
