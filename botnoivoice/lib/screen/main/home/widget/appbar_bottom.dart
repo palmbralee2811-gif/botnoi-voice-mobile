91b2c88e13ef6a4445f3435540754cc842e0eb98
Apisit Suksri <apisit.suk@student.mahidol.ac.th>
2025-07-03 14:58:06 +0700

    เพิ่มขนาดอักษรตรงชื่อภาษา
    เพิ่มขนาดอักษรตรงชื่อภาษา จาก 7:10 เป็น 8:14 ให้เท่ากับ change

--- botnoivoice/lib/screen/main/home/widget/appbar_bottom.dart
+++ botnoivoice/lib/screen/main/home/widget/appbar_bottom.dart
@@ -174,8 +174,8 @@ class _AppBarBottomState extends State<AppBarBottom> {
                       nationalFlagName!,
                       style: GoogleFonts.prompt(
                         fontSize: ResponsiveDesignOrientation.isLandscape
-                            ? 7.sp
-                            : 10.sp,
+                            ? 8.sp
+                            : 14.sp,
                         color: kDark,
                       ),
                     ),
