126d5b5b131b4e5df6dd8a14b27c682b96670eef
Kawinphop Chomnikorn <kawinphop.chomnikorn@gmail.com>
2025-09-18 10:39:59 +0700

    Switch LINE login and API configs to staging environment
    Updated AndroidManifest.xml, api_key_config.dart, and api_url_config.dart to use staging LINE login keys and API URLs instead of production. This change is likely for development or testing purposes.

--- botnoivoice/lib/config/api_key_config.dart
+++ botnoivoice/lib/config/api_key_config.dart
@@ -1,14 +1,15 @@
 // LINE Login Key for Production Mode
-String _lineSdkChannelIdProduction = "1656375389";
+// String _lineSdkChannelIdProduction = "1656375389";
 
 // LINE Login Key for Staging Mode
-// String _lineSdkChannelIdStaging = "2003460314";
+String _lineSdkChannelIdStaging = "2003460314";
 
 String _emailUsernameApiKey = "Ym90b25vaQ";
 String _appleRevenueCatApiKey = "appl_mImKODlZTfTPWctkaQrVLpkMUuz";
 String _googleRevenueCatApiKey = "goog_sNwjgbXxjhOpDHOUHrgBMmYuDID";
 
-String get lineSdkChannelId => _lineSdkChannelIdProduction;
+// String get lineSdkChannelId => _lineSdkChannelIdProduction;
+String get lineSdkChannelId => _lineSdkChannelIdStaging;
 String get emailUsernameApiKey => _emailUsernameApiKey;
 String get appleRevenueCatApiKey => _appleRevenueCatApiKey;
 String get googleRevenueCatApiKey => _googleRevenueCatApiKey;
