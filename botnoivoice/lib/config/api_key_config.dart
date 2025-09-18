f6e2fc84e95d0b4c950d674e46740f8e56fe21ed
Kawinphop Chomnikorn <kawinphop.chomnikorn@gmail.com>
2025-09-18 09:12:55 +0700

    Refactor LINE SDK channel ID variable names
    Renamed LINE SDK channel ID variables to clarify production and staging modes. Updated the getter to use the production channel ID variable.

--- botnoivoice/lib/config/api_key_config.dart
+++ botnoivoice/lib/config/api_key_config.dart
@@ -1,14 +1,14 @@
 // LINE Login Key for Production Mode
-String _lineSdkChannelId = "1656375389";
+String _lineSdkChannelIdProduction = "1656375389";
 
 // LINE Login Key for Staging Mode
-// String _lineSdkChannelId = "2003460314";
+// String _lineSdkChannelIdStaging = "2003460314";
 
 String _emailUsernameApiKey = "Ym90b25vaQ";
 String _appleRevenueCatApiKey = "appl_mImKODlZTfTPWctkaQrVLpkMUuz";
 String _googleRevenueCatApiKey = "goog_sNwjgbXxjhOpDHOUHrgBMmYuDID";
 
-String get lineSdkChannelId => _lineSdkChannelId;
+String get lineSdkChannelId => _lineSdkChannelIdProduction;
 String get emailUsernameApiKey => _emailUsernameApiKey;
 String get appleRevenueCatApiKey => _appleRevenueCatApiKey;
 String get googleRevenueCatApiKey => _googleRevenueCatApiKey;
