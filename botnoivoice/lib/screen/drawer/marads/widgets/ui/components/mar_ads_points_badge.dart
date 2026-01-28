f1bb608e3c4077be6de74385a0ea9af1b126d694
Kawinphop Chomnikorn <kawinphop.chomnikorn@gmail.com>
2026-01-29 00:49:00 +0700

    Make MarAdsPointsBadge open payment dialog on tap
    Wrapped the MarAdsPointsBadge widget with a GestureDetector to show the payment dialog when tapped. This enhances user interaction by allowing users to access payment options directly from the badge.

--- botnoivoice/lib/screen/drawer/marads/widgets/ui/components/mar_ads_points_badge.dart
+++ botnoivoice/lib/screen/drawer/marads/widgets/ui/components/mar_ads_points_badge.dart
@@ -1,3 +1,4 @@
+import 'package:botnoivoice/shared/dialog/payment/payment_dialog.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:flutter_svg/svg.dart';
@@ -10,43 +11,48 @@ class MarAdsPointsBadge extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: EdgeInsets.all(4.w),
-      decoration: BoxDecoration(
-        color: Colors.white,
-        borderRadius: BorderRadius.circular(100.r),
-        boxShadow: [
-          BoxShadow(
-            color: Colors.black.withOpacity(0.1),
-            blurRadius: 8,
-            offset: const Offset(0, 2),
-          ),
-        ],
-      ),
-      child: Row(
-        mainAxisSize: MainAxisSize.min,
-        children: [
-          Padding(
-            padding: EdgeInsets.only(left: 4.w),
-            child: SvgPicture.asset(
-              'assets/images/logo/credit-icon.svg',
-              width: 20.w,
-              height: 20.h,
+    return GestureDetector(
+      onTap: () {
+        showPaymentDialog(context); 
+      },
+      child: Container(
+        padding: EdgeInsets.all(4.w),
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(100.r),
+          boxShadow: [
+            BoxShadow(
+              color: Colors.black.withOpacity(0.1),
+              blurRadius: 8,
+              offset: const Offset(0, 2),
             ),
-          ),
-          SizedBox(width: 4.w),
-          Padding(
-            padding: EdgeInsets.only(right: 4.w),
-            child: Text(
-              points,
-              style: GoogleFonts.prompt(
-                fontSize: 12.sp,
-                fontWeight: FontWeight.bold, // ใช้ Bold ให้เหมือน App Bar
-                color: const Color(0xFF262626), // หรือ kDark
+          ],
+        ),
+        child: Row(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Padding(
+              padding: EdgeInsets.only(left: 4.w),
+              child: SvgPicture.asset(
+                'assets/images/logo/credit-icon.svg',
+                width: 20.w,
+                height: 20.h,
               ),
             ),
-          ),
-        ],
+            SizedBox(width: 4.w),
+            Padding(
+              padding: EdgeInsets.only(right: 4.w),
+              child: Text(
+                points,
+                style: GoogleFonts.prompt(
+                  fontSize: 12.sp,
+                  fontWeight: FontWeight.bold,
+                  color: const Color(0xFF262626),
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
